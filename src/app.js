6e0d53ad8110625028a5955361ebc8af1167aa10
parjoona <parjoona@hotmail.com>
2019-01-09 22:29:11 +0100

    added router-dom for react

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 import 'normalize.css/normalize.css'
 import './styles/main.scss'
 
-const App = () => <p>Hello</p>
+const ExpenseDashboard = () => <p>Hello</p>
+const AddExpense = () => <p>Create expense</p>
+const EditExpense = () => <p>Edit expense</p>
+const Help = () => <p>Get help</p>
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" exact={true} component={ExpenseDashboard}/>
+            <Route path="/create" component={AddExpense}/>
+            <Route path="/edit" component={EditExpense}/>
+            <Route path="/help" component={Help}/>
+        </div>
+    </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'))
