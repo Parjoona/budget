9dbf0683e4f9630286a48d1f628822f5784b7432
parjoona <parjoona@hotmail.com>
2019-01-13 14:23:56 +0100

    adding filters to expenseList

--- src/app.js
+++ src/app.js
@@ -12,17 +12,12 @@ import './styles/main.scss'
 import configureStore from './store/configureStore'
 import { addExpense } from './store/actions/expenses'
 import { setTextFilter } from './store/actions/filters' 
-import getVisibleExpenses from './store/selectors/expenses'
 
 const store = configureStore()
 
 store.dispatch(addExpense({ description: 'Water bill' }))
 store.dispatch(addExpense({ description: 'Gas bill' }))
 store.dispatch(setTextFilter('bill'))
-
-const state = store.getState()
-const visExp = getVisibleExpenses(state.expenses, state.filters)
-console.log(visExp)
 
 ReactDOM.render((
     <Provider store={store}>
