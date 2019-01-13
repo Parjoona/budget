12b077fca0a5c9571f27cd5f1a2459a8fb8218e1
parjoona <parjoona@hotmail.com>
2019-01-13 12:19:59 +0100

    transfer redux store to project

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-
-
 import AppRouter from './routers/AppRouter'
+
+import configureStore from './store/configureStore'
 
 // CSS
 import 'normalize.css/normalize.css'
 import './styles/main.scss'
+
+const store = configureStore()
 
 ReactDOM.render((
     <BrowserRouter>
