30bc9b9a29cb8735d8879f4d5edbb5b242c7afcd
parjoona <parjoona@hotmail.com>
2019-01-17 22:40:27 +0100

    setup enzyme for shallow rendering

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import ReactShallowRenderer from 'react-test-renderer/shallow'
+import { shallow } from 'enzyme'
+
+// import ReactShallowRenderer from 'react-test-renderer/shallow'
+
 import Header from '../../components/Header'
 
 ///////////////////////////////////////////
 /////// Shallow rendering
-
-test('Should render correctly', () => {
-    const renderer = new ReactShallowRenderer()
-
-    renderer.render(<Header />)
-
-    expect(renderer.getRenderOutput()).toMatchSnapshot()
-})
-
-///////////////////////////////////////////
+///////
 /////// Snapshot rendering
 // Tracks changes in data from components
 
+test('Should render correctly', () => {
+    // const renderer = new ReactShallowRenderer()
+    // renderer.render(<Header />)
+    // expect(renderer.getRenderOutput()).toMatchSnapshot()
+
+    const wrapper = shallow(<Header/>)
+    // expect(wrapper.find('h1').length).toBe(1)
+    expect(wrapper).toMatchSnapshot()
+})
