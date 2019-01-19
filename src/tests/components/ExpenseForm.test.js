8ab38df2b9d3036ed947023e6ce02b68bd13f781
parjoona <parjoona@hotmail.com>
2019-01-19 18:58:02 +0100

    aded tests for text input and textarea in expenseform

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -5,14 +5,58 @@ import ExpenseForm from '../../components/ExpenseForm'
 
 import expense from '../store/fixtures/expenses'
 ////////////////////////////////
-//// Mocking libraries: moment()
+//// Mocking libraries: moment() __mocks__
 
-test('Should render ExpenseForm', () => {
-  const wrapper = shallow(<ExpenseForm />)
-  expect(wrapper).toMatchSnapshot()
+describe('ExpenseForm rendering', () => {
+  test('Should render ExpenseForm', () => {
+    const wrapper = shallow(<ExpenseForm />)
+    expect(wrapper).toMatchSnapshot()
+  })
+  
+  test('Should render ExpenseForm with expense data', () => {
+    const wrapper = shallow(<ExpenseForm expense={expense[0]} />)
+    expect(wrapper).toMatchSnapshot()
+  })
 })
 
-test('Should render ExpenseForm with expense data', () => {
-  const wrapper = shallow(<ExpenseForm expense={expense[0]} />)
-  expect(wrapper).toMatchSnapshot()
+describe('ExpenseForm interactions', () => {
+  test('Should render error for invalid submission', () => {
+    const wrapper = shallow(<ExpenseForm />)
+    wrapper
+      .find('form')
+      .simulate('submit', {
+        preventDefault: () => {}
+      })
+
+      expect(wrapper.state('error').length)
+        .toBeGreaterThan(0)
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  test('Should set description on input change', () => {
+    const value = "new description"
+    const wrapper = shallow(<ExpenseForm />)
+    wrapper.find('input')
+      .at(0)
+      .simulate('change', {
+        target: { value }
+      })
+
+      expect(wrapper.state('description')).toBe(value)
+  })
+  
+  test('Should set textarea on input change', () => {
+    const value = "new note"
+    const wrapper = shallow(<ExpenseForm />)
+    wrapper.find('textarea')
+      .at(0)
+      .simulate('change', {
+        target: { value }
+      })
+
+      expect(wrapper.state('note')).toBe(value)
+  })
 })
+
+
