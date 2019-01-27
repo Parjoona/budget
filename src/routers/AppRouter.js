5b07dd047bf51a1abdbb4250e5a681c8247cc4ba
parjoona <parjoona@hotmail.com>
2019-01-27 22:37:29 +0100

    created privateRoute for unauthenticated users

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,23 +2,22 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import Header from '../components/Header'
-
 import LoginPage from '../components/LoginPage'
 import ExpenseDashboard from '../components/ExpenseDashboard'
 import AddExpense from '../components/AddExpensePage'
 import EditExpense from '../components/EditExpensePage'
 import Help from '../components/Help'
 import NotFound from '../components/404'
+import PrivateRoute from './PrivateRoute'
 
 const AppRouter = () => (
     <div>
         <Header />
         <Switch>
             <Route path="/" exact={true} component={LoginPage}/>
-            <Route path="/dashboard" component={ExpenseDashboard}/>
-            <Route path="/create" component={AddExpense}/>
-            <Route path="/edit/:id" component={EditExpense}/>
+            <PrivateRoute path="/dashboard" component={ExpenseDashboard}/>
+            <PrivateRoute path="/create" component={AddExpense}/>
+            <PrivateRoute path="/edit/:id" component={EditExpense}/>
             <Route path="/help" component={Help}/>
             <Route component={NotFound}/>
         </Switch>
