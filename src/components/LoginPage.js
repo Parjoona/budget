5c3671c5ee092b3ed727ff4710b07640e4194c06
parjoona <parjoona@hotmail.com>
2019-01-28 22:19:13 +0100

    added style for button

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,9 +6,9 @@ import { startLogin } from '../store/actions/auth'
 export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
-      <h1>Budget application</h1>
-      <p>Time to get expenses in order</p>
-      <button onClick={startLogin}>Login</button>
+      <h1 className="box-layout__title">Budget application</h1>
+      <p className="box-layout__sub-title">Time to get expenses in order</p>
+      <button className="button__login" onClick={startLogin}>Login using Google</button>
     </div>
   </div>
 )
